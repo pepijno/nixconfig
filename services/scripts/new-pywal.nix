d2846db4fb29489af4ad6ec8c6596cd96867749f
Pepijn Overbeeke <pepijn.overbeeke@gmail.com>
2021-01-18 22:08:21 +0100

    Changed wal script to run more often

--- services/scripts/new-pywal.nix
+++ services/scripts/new-pywal.nix
@@ -10,17 +10,20 @@ let
 in
   pkgs.writeScriptBin "new-pywal" ''
     #!${pkgs.stdenv.shell}
-    currenttime=$(${date} +%H:%M)
-    params=()
-    if [[ "$currenttime" < "20:00" ]] && [[ "$currenttime" > "08:00" ]]; then
-      params+=(-l -a 86)
-    else
-      params+=(-a 94)
+    currentminute=$(${date} +%M)
+    if [[ "$currentminute" == "00" ]]; then
+      currenttime=$(${date} +%H:%M)
+      params=()
+      if [[ "$currenttime" < "20:00" ]] && [[ "$currenttime" > "08:00" ]]; then
+        params+=(-l -a 86)
+      else
+        params+=(-a 94)
+      fi
+      ${pkgs.pywal}/bin/wal \
+        -i ${config.home.homeDirectory}/Pictures/Wallpapers/ \
+        -o ${restart-dunst}/bin/restart-dunst \
+        "''${params[@]}"
     fi
-    ${pkgs.pywal}/bin/wal \
-      -i ${config.home.homeDirectory}/Pictures/Wallpapers/ \
-      -o ${restart-dunst}/bin/restart-dunst \
-      "''${params[@]}"
     ${pkgs.pywal}/bin/wal -R
     source ${config.home.homeDirectory}/.cache/wal/colors.sh
     ${pkgs.betterlockscreen}/bin/betterlockscreen -u "$wallpaper"
--- services/wal.nix
+++ services/wal.nix
@@ -24,7 +24,7 @@ in {
     };
 
     Timer = {
-      OnUnitInactiveSec = "2700s";
+      OnUnitInactiveSec = "10s";
     };
 
     Install = {
