b7cc4303b54c3710984c8dfb16764281a8293c84
Pepijn Overbeeke <pepijn.overbeeke@gmail.com>
2021-01-18 22:17:41 +0100

    Add alacritty as terminal as it has better unicode support and wayland

---
+++ alacritty.nix
@@ -0,0 +1,56 @@
+{ pkgs, ... }:
+
+{
+  programs.alacritty = {
+    enable = true;
+    settings = {
+      env.TERM = "xterm-256color";
+
+      window = {
+        padding = {
+          x = 25;
+          y = 20;
+        };
+        dynamic_padding = true;
+        decorations = "none";
+        startup_mode = "Maximized";
+      };
+
+      scrolling = {
+        history = 10000;
+        multiplier = 3;
+      };
+
+      font = {
+        normal = {
+          family = "DejaVu Sans Mono";
+          style = "Light";
+        };
+        bold = {
+          family = "DejaVu Sans Mono";
+          style = "Light";
+        };
+        italic = {
+          family = "DejaVu Sans Mono";
+          style = "Light";
+        };
+        size = 11.0;
+      };
+
+      draw_bold_text_with_bright_colors = false;
+
+      bell = {
+        duration = 0;
+      };
+
+      background_opacity = 0.94;
+
+      cursor = {
+        style = "Block";
+        unfocused_hollow = true;
+      };
+
+      live_config_reload = true;
+    };
+  };
+}
