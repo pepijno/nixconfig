50398de05e8b5f953c88e3f733a7492727daac11
Pepijn Overbeeke <Pepijn.Overbeeke@triodos.com>
2021-02-08 09:47:25 +0100

    Add extra git config

--- applications/git.nix
+++ applications/git.nix
@@ -10,7 +10,11 @@
       a = "add -p";
       ci = "commit";
       co = "checkout";
-      lola = "log --graph --decorate --pretty=online --abbrev-commit --all --date-order";
+      s = "stash --include-untracked";
+      sl = "stash list";
+      sp = "stash pop";
+      lol = "log --graph --abbrev-commit --decorate --first-parent --date-order --format=format:'%C(bold blue)%h%C(reset) - %C(bold cyan)%aD%C(reset) %C(bold green)(%ar)%C(reset)%C(auto)%d%C(reset)%n''          %C(white)%s%C(reset) %C(dim white)- %an%C(reset)'";
+      lola = "log --graph --decorate --abbrev-commit --all --date-order --format=format:'%C(bold blue)%h%C(reset) - %C(bold cyan)%aD%C(reset) %C(bold green)(%ar)%C(reset)%C(auto)%d%C(reset)%n''          %C(white)%s%C(reset) %C(dim white)- %an%C(reset)'";
     };
   };
 }
