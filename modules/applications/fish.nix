01ee2bafffc1ad03c6ae36043dc5ac20749bceac
Pepijn Overbeeke <pepijn.overbeeke@gmail.com>
2022-03-26 23:58:18 +0100

    Add fish plugin

--- modules/applications/fish.nix
+++ modules/applications/fish.nix
@@ -6,12 +6,23 @@
 
     functions = {
       fish_prompt = "
-      set -l nix_shell_info (
-        if test -n \"$IN_NIX_SHELL\"
-          echo -n \" <nix-shell> \"
-        end
-      )
-      echo -n -s \\((set_color green)(date '+%T')(set_color white)\\)-\\((set_color yellow)(prompt_pwd)(set_color white)\\)-(set_color red)\"$nix_shell_info❯\"(set_color yellow)'❯'(set_color blue)'❯ '";
+        set -l nix_shell_info (
+          if test -n \"$IN_NIX_SHELL\"
+            echo -n \" <nix-shell> \"
+          end
+        )
+        echo -n -s \\((set_color green)(date '+%T')(set_color white)\\)-\\((set_color yellow)(prompt_pwd)(set_color white)\\)-(set_color red)\"$nix_shell_info❯\"(set_color yellow)'❯'(set_color blue)'❯ '
+      ";
+      fish_right_prompt = ''
+        set -l color_green  (set_color green)
+        set -l color_dim    (set_color white)
+        set -l color_off    (set_color -o normal)
+
+        echo -n -s $color_dim (date +%H$color_green:$color_dim%M$color_green:$color_dim%S)$color_off
+      '';
+      fish_title = ''
+        echo "$PWD | $_" | sed "s|$HOME|~|g"
+      '';
     };
 
     shellAliases = {
@@ -26,5 +37,17 @@
         cat ~/.cache/wal/sequences
       end
     '';
+
+    plugins = [
+      {
+        name = "nix-env";
+        src = pkgs.fetchFromGitHub {
+          owner = "lilyball";
+          repo = "nix-env.fish";
+          rev = "7b65bd228429e852c8fdfa07601159130a818cfa";
+          sha256 = "sha256-RG/0rfhgq6aEKNZ0XwIqOaZ6K5S4+/Y5EEMnIdtfPhk=";
+        };
+      }
+    ];
   };
 }
