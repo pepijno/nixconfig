10a2b31a6ab5ae70e809052de66159a2d5fe0b93
Pepijn Overbeeke <pepijn.overbeeke@gmail.com>
2022-03-26 12:26:29 +0100

    Add cpp.lua

---
+++ modules/applications/neovim/nvim/after/ftplugin/cpp.lua
@@ -0,0 +1,4 @@
+require("configs.lsp.handlers").load_server("clangd")
+vim.opt.tabstop = 2 -- Number of space in a tab
+vim.opt.shiftwidth = 2 -- Number of space inserted for indentation
+vim.opt.expandtab = true -- Enable the use of space in tab
