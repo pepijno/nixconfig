df51e23824a76291e853efe67187fdd4a1cac4b1
Pepijn Overbeeke <pepijn.overbeeke@gmail.com>
2022-03-28 11:58:55 +0200

    Update module better lock screen

--- modules/services/betterlockscreen.nix
+++ modules/services/betterlockscreen.nix
@@ -1,30 +1,15 @@
 { config, pkgs, ... }:
 
-let
-  sleep = "/run/current-system/sw/bin/sleep";
-in
 {
-  # services.betterlockscreen = {
-  #   enable = true;
-  #   inactiveInterval = 20;
-  # };
 
-  systemd.user.services.betterlockscreen = {
-    Unit = {
-      Description = "Lock screen when going to sleep/suspend";
-      Before = [ "sleep.target" "suspend.target" "hibernate.target" "hybrid-sleep.target" ];
-    };
-
-    Service = {
-      Type = "forking";
-      Environment = "DISPLAY:=0";
-      ExecStart = "${pkgs.betterlockscreen}/bin/betterlockscreen --lock";
-      TimeoutSec = "infinity";
-      ExecStartPost = "${sleep} 1";
-    };
-
-    Install = {
-      WantedBy = [ "sleep.target" "suspend.target" "hibernate.target" "hybrid-sleep.target" ];
+  services.screen-locker = {
+    enable = true;
+    inactiveInterval = 5;
+    lockCmd = "${pkgs.betterlockscreen}/bin/betterlockscreen -l dim";
+    xautolock = {
+      extraOptions = [
+        "Xautolock.killer: systemctl suspend"
+      ];
     };
   };
 }
