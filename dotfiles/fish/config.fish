6cd445ab4815d9bcfa20cf75604c503de9bc150e
Pepijn Overbeeke <pepijn.overbeeke@gmail.com>
2025-07-28 22:25:42 +0200

    Make fzf use catppuccin

--- dotfiles/fish/config.fish
+++ dotfiles/fish/config.fish
@@ -5,6 +5,12 @@ set -g __fish_config_sourced 1
 set --export EDITOR "nvim -f"
 set -U fish_greeting
 set FZF_DEFAULT_COMMAND "rg --files"
+set -Ux FZF_DEFAULT_OPTS "\
+    --color=bg+:#CCD0DA,bg:#EFF1F5,spinner:#DC8A78,hl:#D20F39 \
+    --color=fg:#4C4F69,header:#D20F39,info:#8839EF,pointer:#DC8A78 \
+    --color=marker:#7287FD,fg+:#4C4F69,prompt:#8839EF,hl+:#D20F39 \
+    --color=selected-bg:#BCC0CC \
+    --color=border:#CCD0DA,label:#4C4F69"
 
 set -gx XDG_CACHE_HOME "$HOME/.cache"
 set -gx XDG_CONFIG_HOME "$HOME/.config"
